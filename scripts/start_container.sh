d9ec9c58bb111234702db8311647aee35d1f50b1
AmarBoggavarapu <121476723+AmarBoggavarapu@users.noreply.github.com>
2025-04-26 20:08:37 -0400

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull amarbvn/awsprac-ci-proj-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 amarbvn/awsprac-ci-proj-app
+docker run -d -p 5000:8000 amarbvn/awsprac-ci-proj-app
